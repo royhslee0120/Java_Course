cf1915f149da2ada267dbbba269f338dd358a813
royhsle0120 <royhslee0120@gmail.com>
2026-01-06 22:00:06 +0900

    chapter3 완료

---
+++ src/chapter3/lambda/Calculator.java
@@ -0,0 +1,8 @@
+package chapter3.lambda;
+
+@FunctionalInterface
+public interface Calculator {
+
+    int sum(int a, int b);
+
+}
---
+++ src/chapter3/thread/runnable/MyNewClass.java
@@ -0,0 +1,8 @@
+package chapter3.thread.runnable;
+
+public class MyNewClass {
+
+    public void printMessage() {
+        System.out.println("MyNewClass의 기능을 실행합니다.");
+    }
+}
